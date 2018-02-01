35ffa04a53958a51ea4f18638e6bf9933d378b9f
Strosel <>
2018-02-01 13:40:53 +0100

    Improved function

--- lambda_function.py
+++ lambda_function.py
@@ -29,7 +29,6 @@ class VWCarnet(object):
         # Fake the VW CarNet mobile app headers
         self.headers = { 'Accept': 'application/json, text/plain, */*', 'Content-Type': 'application/json;charset=UTF-8', 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; D5803 Build/23.5.A.1.291; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/63.0.3239.111 Mobile Safari/537.36' }
         self.session = requests.Session()
-        self.timeout_counter = 30 # seconds
 
         try:
             self._carnet_logon()
@@ -259,7 +258,7 @@ class VWCarnet(object):
 def main(event, context):
 
     vw = VWCarnet(event)
-    if vw.output != "":
+    if vw.output == "":
         vw._carnet_do_action()
     return {
         "isBase64Encoded": False,
